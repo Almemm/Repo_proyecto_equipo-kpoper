d8d204c1cb628cb6af106881601d19740c68180c
Almemm <almendra.mendez@uc.cl>
2025-06-27 00:32:49 -0400

    Ya no sé

--- entrega_05/docs/script.js
+++ entrega_05/docs/script.js
@@ -1,18 +1,3 @@
-let indice = 0;
 
-function cambiarImagen(direccion) {
-  const imagenes = document.querySelectorAll('.imagenescarrusel img');
-  const total = imagenes.length;
 
-  indice += direccion;
 
-  if (indice < 0) {
-    indice = total - 1;
-  } else if (indice >= total) {
-    indice = 0;
-  }
-
-  const carrusel = document.querySelector('.imagenescarrusel');
-  const desplazamiento = -indice * 100;
-  carrusel.style.transform = `translateX(${desplazamiento}%)`;
-}
